a7124632ba46d92db7fe6e7c4043dc16fe9cdf65
for-covid-19 <18895375022@139.com>
2020-05-29 14:51:15 +0800

    创建 cell_layout

--- app/src/main/java/com/example/myble/activity/show_result_activity.java
+++ app/src/main/java/com/example/myble/activity/show_result_activity.java
@@ -9,8 +9,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myble.R;
+import com.example.myble.adapter.MyAdapter;
 import com.example.myble.databinding.ActivityShowResultBinding;
 import com.example.myble.room.Word;
 import com.example.myble.viewmodel.WordViewModel;
@@ -22,32 +25,64 @@ public class show_result_activity extends AppCompatActivity {
     ActivityShowResultBinding binding;
     private WordViewModel wordViewModel;
 
+    RecyclerView recyclerView;
+    MyAdapter myAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_show_result);
         wordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
-
         binding.setLifecycleOwner(this);
 
-        textView = findViewById(R.id.display);
+        recyclerView = findViewById(R.id.recyclerView);
+        myAdapter = new MyAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myAdapter);
+
         wordViewModel.getAllWordsLive().observe(this, new Observer<List<Word>>() {
             @Override
             public void onChanged(List<Word> words) {
-                StringBuilder text = new StringBuilder();
-                for (int i = 0; i < words.size(); i++) {
-                    Word word = words.get(i);
-                    text.append(word.getId()).append(":").append(word.getWord()).append("=").append(word.getChineseMeaning()).append("\n");
-                }
-                textView.setText(text.toString());
+                myAdapter.setAllWords(words);
+                myAdapter.notifyDataSetChanged();
             }
         });
         findViewById(R.id.insert).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Word word1 = new Word("hello", "您好");
-                Word word2 = new Word("world", "世界");
-                wordViewModel.insertWords(word1, word2);
+//                Word word1 = new Word("hello", "您好");
+//                Word word2 = new Word("world", "世界");
+                String[] english = {
+                        "Hello",
+                        "World",
+                        "Android",
+                        "Google",
+                        "Studio",
+                        "Project",
+                        "Database",
+                        "Recycler",
+                        "View",
+                        "String",
+                        "Value",
+                        "Integer"
+                };
+                String[] chinese = {
+                        "你好",
+                        "世界",
+                        "安卓系统",
+                        "谷歌公司",
+                        "工作室",
+                        "项目",
+                        "数据库",
+                        "回收站",
+                        "视图",
+                        "字符串",
+                        "价值",
+                        "整数类型"
+                };
+                for (int i = 0; i < english.length; i++) {
+                    wordViewModel.insertWords(new Word(english[i], chinese[i]));
+                }
             }
         });
 
---
+++ app/src/main/java/com/example/myble/adapter/MyAdapter.java
@@ -0,0 +1,63 @@
+package com.example.myble.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myble.R;
+import com.example.myble.room.Word;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 聊表的内容管理器 如何填充数据
+ * 使用 MyAdapter.MyViewHolder
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    List<Word> allWords = new ArrayList<>();
+
+    public void setAllWords(List<Word> allWords) {
+        this.allWords = allWords;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+//        View itemView = layoutInflater.inflate(R.layout.cell_normal, parent, false);
+//        return new MyViewHolder(itemView);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Word word = allWords.get(position);
+        holder.textViewNumber.setText(String.valueOf(word.getId()));
+        holder.textViewEnglish.setText(word.getWord());
+        holder.textViewChinese.setText(word.getChineseMeaning());
+    }
+
+    @Override
+    public int getItemCount() {
+        return allWords.size();
+    }
+
+    /**
+     * 创建自定义的viewholder 类
+     * */
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewNumber, textViewEnglish, textViewChinese;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewNumber = itemView.findViewById(R.id.textViewNumber);
+            textViewEnglish = itemView.findViewById(R.id.textViewEnglish);
+            textViewChinese = itemView.findViewById(R.id.textViewChinese);
+        }
+    }
+}
